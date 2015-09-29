c200534d4d32b071ee9a2ffe66da6eb4f3042c10
dmatis <darren.matis@gmail.com>
2015-09-29 10:27:07 -0700

    Implementation of Rotate Matrix added

--- src/AdjacencyListsGraph.java
+++ src/AdjacencyListsGraph.java
@@ -1,5 +1,36 @@
-/**
- * Created by darren on 2015-09-29.
- */
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class AdjacencyListsGraph {
+
+    private Map<Integer, List<Integer>> adjacencyList;
+
+    public AdjacencyListsGraph(int numVertices) {
+        adjacencyList = new HashMap<Integer, List<Integer>>();
+        for (int i = 1; i <= numVertices; i++) {
+            adjacencyList.put(i, new LinkedList<Integer>());
+        }
+    }
+
+    public void setEdge(int source, int dest) {
+        if (source > adjacencyList.size() || dest > adjacencyList.size())
+        {
+            System.out.println("Invalid vertex entered");
+            return;
+        }
+        List<Integer> sourcelist = adjacencyList.get(source);
+        sourcelist.add(dest);
+        List<Integer> destlist = adjacencyList.get(dest);
+        destlist.add(source);
+    }
+
+    public List<Integer> getEdge(int source) {
+        if (source > adjacencyList.size()) {
+            System.out.println("Invalid vertex entered");
+            return null;
+        }
+        return adjacencyList.get(source);
+    }
 }
--- src/RotateMatrix.java
+++ src/RotateMatrix.java
@@ -1,5 +1,57 @@
-/**
- * Created by darren on 2015-09-29.
- */
+import java.util.Arrays;
+
 public class RotateMatrix {
+
+    /*
+         top
+      ********* r
+    l *&&&&&&&* i
+    e *&%%%%%&* g
+    f *&&&&&&&* h
+    t ********* t
+        bottom
+     */
+
+    // Rotates a matrix of size n clockwise
+    public static void rotate(int[][] matrix, int n) {
+        for (int layer = 0; layer < n/2; layer++) {
+            int first = layer;
+            int last = n - layer - 1;
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+
+                // saves the top part
+                int top = matrix[first][i];
+                // left moves to top
+                matrix[first][i] = matrix[last-offset][first];
+                // bottom moves to left
+                matrix[last-offset][first] = matrix[last][last-offset];
+                // right moves to the bottom
+                matrix[last][last-offset] = matrix[i][last];
+                // top moves to right
+                matrix[i][last] = top;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] one = {1,2,3,4};
+        int[] two = {5,6,7,8};
+        int[] three = {9,10,11,12};
+        int[] four = {13,14,15,16};
+
+        int[][] fourMatrix = {one,two,three,four};
+
+        for (int i = 0; i < fourMatrix.length; i++) {
+            System.out.println(Arrays.toString(fourMatrix[i]));
+        }
+
+        System.out.println("-------AFTER ROTATION-----------");
+
+        rotate(fourMatrix, 4);
+
+        for (int i = 0; i < fourMatrix.length; i++) {
+            System.out.println(Arrays.toString(fourMatrix[i]));
+        }
+    }
 }

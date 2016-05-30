1340b80f12bdb8f67931123d842258acc5c8a1ab
dmatis <darren.matis@gmail.com>
2016-05-30 15:25:42 -0700

    constructed a breadthfirst traversal algorithm

---
+++ src/BreadthFirstTree.java
@@ -0,0 +1,73 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BreadthFirstTree {
+
+    Vertex root;
+
+    public BreadthFirstTree(Vertex root) {
+        this.root = root;
+    }
+
+    public void traverse(Vertex start) {
+        Queue<Vertex> nextVertices = new LinkedList<Vertex>();
+        nextVertices.add(start);
+        while (!nextVertices.isEmpty()) {
+            Vertex currVertex = nextVertices.remove();
+            currVertex.visit();
+
+            Iterator<Vertex> iter = currVertex.adjVertices.iterator();
+            while (iter.hasNext()) {
+                //Add all adjacent vertices to Queue
+                Vertex currAdjVertex = iter.next();
+                if (!currAdjVertex.visited && !nextVertices.contains(currAdjVertex)) {
+                    nextVertices.add(currAdjVertex);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Vertex a = new Vertex(1);
+        Vertex b = new Vertex(2);
+        Vertex c = new Vertex(3);
+        Vertex d = new Vertex(4);
+        Vertex e = new Vertex(5);
+        Vertex f = new Vertex(6);
+        Vertex g = new Vertex(7);
+        Vertex h = new Vertex(8);
+
+        a.addAdjacent(b);
+        a.addAdjacent(d);
+        a.addAdjacent(g);
+
+        b.addAdjacent(e);
+        b.addAdjacent(a);
+        b.addAdjacent(f);
+
+        c.addAdjacent(f);
+        c.addAdjacent(h);
+
+        d.addAdjacent(a);
+        d.addAdjacent(f);
+
+        e.addAdjacent(b);
+        e.addAdjacent(g);
+
+        f.addAdjacent(b);
+        f.addAdjacent(c);
+        f.addAdjacent(d);
+
+        g.addAdjacent(a);
+        g.addAdjacent(e);
+
+        BreadthFirstTree tree = new BreadthFirstTree(a);
+        tree.traverse(a);
+    }
+}
+
+
+
+
